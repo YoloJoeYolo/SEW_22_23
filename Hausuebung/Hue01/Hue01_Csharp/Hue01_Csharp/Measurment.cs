76030b14814335afef08ab907b96ef748c8d40b6
YoloJoeYolo <johannes.werner05@gmail.com>
2022-11-06 15:35:10 +0100

    HUe01

---
+++ Hausuebung/Hue01/Hue01_Csharp/Hue01_Csharp/Measurment.cs
@@ -0,0 +1,26 @@
+﻿namespace Hue01_Csharp
+{
+    internal class Measurment
+    {
+        DateTime time;
+        double value;
+
+        public Measurment(DateTime time, double value)
+        {
+            this.time = time;
+            this.value = value;
+        }
+
+        public DateTime Time
+        {
+            get{return time;}
+            set{time = value;}
+        }
+
+        public Double Value
+        {
+            get{return value;}
+            set{this.value = value;}
+        }
+    }
+}
