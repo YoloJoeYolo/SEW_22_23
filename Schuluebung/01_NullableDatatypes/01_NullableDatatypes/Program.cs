abb0f5e4b2262d42eb09d592bdddafc1b37fc470
YoloJoeYolo <johannes.werner05@gmail.com>
2022-11-10 07:53:54 +0100

    01

---
+++ Schuluebung/01_NullableDatatypes/01_NullableDatatypes/Program.cs
@@ -0,0 +1,22 @@
+﻿int i = 100;
+// i = null;// i kann nicht null sein 
+int? j = null; // "noch" kein Wert zugewiesen
+
+Console.WriteLine(j);
+
+j = 200;
+
+Console.WriteLine(j);
+
+if (j.HasValue) // ungleich null
+{
+    i = i * j.Value;
+}
+
+int k = i * j.GetValueOrDefault();
+
+double? v = null; 
+
+string? s = null;
+
+string text = null; 
