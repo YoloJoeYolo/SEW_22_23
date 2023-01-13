b491d646fcdadab0d46f04f3d11152a72f245c2b
YoloJoeYolo <johannes.werner05@gmail.com>
2023-01-13 16:47:01 +0100

    Async

---
+++ Schuluebung/SEW_22_23/12__DownloadClient/PdfDownloader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12__DownloadClient
+{
+    internal class PdfDownloader
+    {
+        public async Task<string> DownloadAsync(Uri url)
+        {
+            using (WebClient client = new WebClient()) 
+            {
+                string pdfName;
+                // http://asdf.at/downloads/document.pdf
+                if (!url.Segments.Last().EndsWith(".pdf")){
+                    pdfName = url.Segments.Last() + ".pdf";      // Segments: einzelne "Teile" der URL durch / getrennt
+
+                }
+                else
+                {
+                    pdfName = url.Segments.Last();
+                }
+                string fileName = @"C:\dev\" + pdfName;
+                Console.WriteLine("Begin Download: " + pdfName);
+                Task t = client.DownloadFileTaskAsync(url, fileName);
+                await t;
+                Console.WriteLine("Download finished: " +pdfName);
+                // hier wird automatisch ein Task Objekt zurückgegeben (macht Compiler)
+                return fileName;    // besser ein event nutzen und darauf warten, wenn man eine aktive Medlung bekommen möchte
+            }
+        }
+    }
+}
---
+++ Schuluebung/SEW_22_23/12__DownloadClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using _12__DownloadClient;
+
+List<Uri> urls = new List<Uri>();
+urls.Add(new Uri("http://bit.ly/leseprobe_schroedinger_cplusplus"));
+urls.Add(new Uri("http://bit.ly/leseprobe_schroedinger_abap"));
+urls.Add(new Uri("http://bit.ly/leseprobe_schroedinger_html5"));
+urls.Add(new Uri("http://bit.ly/leseprobe_programmieren_lernen"));
+urls.Add(new Uri("https://www.thecodingguys.net/resources/cs-cheat-sheet.pdf"));
+
+List<Task<string>> tasks = new List<Task<string>>();
+foreach(Uri url in urls)
+{
+    PdfDownloader downloader = new PdfDownloader();
+    Task<string> t = downloader.DownloadAsync(url);
+    //string fileName = t.Result; // führt dazu das t.Wait aufgerufen wird, da das Result noch nicht da ist
+    tasks.Add(t);
+    //t.Wait();
+};
+
+Task.WaitAll(tasks.ToArray());  // warten bis alle Tasks fertig sind
+tasks.ForEach(task => Console.WriteLine("File can be found: " +task.Result));
+Console.WriteLine("Alle Downloads abgeschlossen!");
