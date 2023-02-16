7c34f1239f6997006e1e480fc27d3c88084c27f1
YoloJoeYolo <johannes.werner05@gmail.com>
2023-02-16 08:32:33 +0100

    2 fertig alles fertig

--- Schuluebung/SEW_22_23/PrimeNumbers_2_2/Program.cs
+++ Schuluebung/SEW_22_23/PrimeNumbers_2_2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PrimeNumbers_2_2;
+
+PrimeNumber primeNumber = new PrimeNumber();
+
+primeNumber.PrimeNumberFound += prim => Console.WriteLine(prim.ToString() +" is a Pprime number!");
+
+Console.WriteLine("Search for prime numbers between 0 and 100 started!");
+Task t = primeNumber.GeneratePrimeNumbersAsync(0, 100);
+
+t.Wait();
+Console.WriteLine("Search for prime numbers between 0 and 100 finished!");
