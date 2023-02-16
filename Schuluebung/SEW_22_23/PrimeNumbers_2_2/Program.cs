cc5fba77f696ce90d875420fe01ba33c361a6d89
YoloJoeYolo <johannes.werner05@gmail.com>
2023-02-16 08:45:52 +0100

    Test fertig

--- Schuluebung/SEW_22_23/PrimeNumbers_2_2/Program.cs
+++ Schuluebung/SEW_22_23/PrimeNumbers_2_2/Program.cs
@@ -1,11 +1,32 @@
 ﻿using PrimeNumbers_2_2;
 
+int startNumber;
+int endNumber;
+
 PrimeNumber primeNumber = new PrimeNumber();
 
-primeNumber.PrimeNumberFound += prim => Console.WriteLine(prim.ToString() +" is a Pprime number!");
+primeNumber.PrimeNumberFound += prim => Console.WriteLine(prim.ToString() +" is a prime number!");
 
-Console.WriteLine("Search for prime numbers between 0 and 100 started!");
-Task t = primeNumber.GeneratePrimeNumbersAsync(0, 100);
+Console.WriteLine("For the search of a prime number you have to define a start and a end number.");
+while (true)
+{
+    Console.Write("Enter a start number: ");
+    if (int.TryParse(Console.ReadLine(), out startNumber))
+    {
+        break;
+    }
+}
+while (true)
+{
+    Console.Write("Enter a end number: ");
+    if (int.TryParse(Console.ReadLine(), out endNumber))
+    {
+        break;
+    }
+}
+
+Console.WriteLine("Search for prime numbers between {0} and {1} started!", startNumber, endNumber);
+Task t = primeNumber.GeneratePrimeNumbersAsync(startNumber, endNumber);
 
 t.Wait();
-Console.WriteLine("Search for prime numbers between 0 and 100 finished!");
+Console.WriteLine("Search for prime numbers between {0} and {1} finished!", startNumber, endNumber);
