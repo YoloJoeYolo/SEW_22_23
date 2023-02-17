0086dfc524178a02b00727e92b7eaffa263b72c6
Felix <Felix@DESKTOP-3GTOHP5>
2023-02-17 15:58:20 +0100

    Bsp2

--- Schuluebung/SEW_22_23/Bsp2/Program.cs
+++ Schuluebung/SEW_22_23/Bsp2/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using Bsp2;
+
+PrimeNumber primeNumber = new PrimeNumber();
+
+primeNumber.PrimeFactorFound += (number) => Console.WriteLine("Es wurde eine Primzahl gefunden: " +number);
+
+primeNumber.GeneratePrimeFactorsAsync(275);
