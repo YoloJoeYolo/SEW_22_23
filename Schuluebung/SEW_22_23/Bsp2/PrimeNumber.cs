7970fc54535edce928adf4ac363422c1bfe0f752
Felix <Felix@DESKTOP-3GTOHP5>
2023-02-17 15:55:51 +0100

    Bsp1

---
+++ Schuluebung/SEW_22_23/Bsp2/PrimeNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bsp2
+{
+    public class PrimeNumber
+    {
+        public Action<int> PrimeFactorFound;
+
+        public async Task GeneratePrimeFactorsAsync(int number)
+        {
+            for (int div = 2; div <= number; div++)
+            {
+                while (number % div == 0)
+                {
+                    if (PrimeFactorFound != null)
+                    {
+                        PrimeFactorFound(div);
+                    }
+
+                    number = number / div;
+                }
+            }
+        }
+    }
+
+
+}
--- Schuluebung/SEW_22_23/Bsp2/Program.cs
+++ Schuluebung/SEW_22_23/Bsp2/Program.cs
@@ -1,14 +1,1 @@
 ﻿
-
-List<IArticle> articles = new List<IArticle>();
-
-articles.Add(new Book(14.99, "A nice C# book"));
-articles.Add(new Smartphone(999.99, "IPhone 1"));
-articles.Add(new Book(20, "C# for pros"));
-articles.Add(new Smartphone(1499.99, "S22"));
-
-double sum = 0;
-
-articles.ForEach(item => sum += item.Price);
-
-Console.WriteLine("Die Summe beträgt: " + sum);
