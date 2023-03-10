ce1e91a2fb9928c0d05b744ccb2010804b8d1df9
YoloJoeYolo <johannes.werner05@gmail.com>
2023-03-10 15:29:04 +0100

    First Webapp

---
+++ Schuluebung/SEW_22_23/13_FirstWebApp/Pages/Person.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _13_FirstWebApp.Pages
+{
+    public class PersonModel : PageModel
+    {
+        public string 
+
+        public void OnGet()
+        {
+        }
+    }
+}
