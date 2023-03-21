f5ea17410bb96100447f18a7fa0e444c1aa0001c
Johannes Werner <johannes.werner05@gmail.com>
2023-03-21 09:11:04 +0100

    SUE14

--- Schuluebung/SEW_22_23/14_SecondWebApp/Pages/Subtraction.cshtml.cs
+++ Schuluebung/SEW_22_23/14_SecondWebApp/Pages/Subtraction.cshtml.cs
@@ -5,8 +5,21 @@ namespace _14_SecondWebApp.Pages
 {
     public class SubtractionModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		public double? Result { get; set; }
+		public string Calculation { get; set; }
+
+
+
+		public void OnGet()
+		{
+		}
+
+		[HttpPost]
+		public IActionResult OnPostCalculateAndRedirect(double minuend, double subtrahend)
+		{
+			double summe = minuend - subtrahend;
+			return RedirectToPage("Result", new { result = summe, calculation = $"{minuend} - {subtrahend} = ", source = "Subtraction" });        //Wir leiten auf die Seite Result weiter            
+																																				  //anonymes Object        
+		}
+	}
 }
