d4d48d15dccb3319ce4f18159176b255f1fa24e4
YoloJoeYolo <johannes.werner05@gmail.com>
2023-03-30 09:22:09 +0200

    Push

---
+++ Hausuebung/Hue05/HUE05/Pages/HikingTrailCalculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HUE05.Pages
+{
+    public class HikingTrailCalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
