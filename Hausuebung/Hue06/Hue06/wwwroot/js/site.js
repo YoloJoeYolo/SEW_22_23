65ecaf38e2dd1de7674db22cf39aea3d8871bc54
Johannes Werner <johannes.werner05@gmail.com>
2023-05-31 18:12:28 +0200

    HUE06

--- Hausuebung/Hue06/Hue06/wwwroot/js/site.js
+++ Hausuebung/Hue06/Hue06/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#tabletasks').DataTable({
+        "scrolly": "450px",
+        "scrollcollapse": true,
+        "paging": true
+    });
+});
